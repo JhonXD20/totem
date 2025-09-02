96f32c80e0127475ae637d5334e0087eb4bac384
João Pedro Almeida Lacerda <sk8.99.jpedro@gmail.com>
2025-09-02 15:26:12 -0300

    criação de carregamento dinamico no template

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const contentArea = document.getElementById("content-area");
+
+    async function loadView(view) {
+        try {
+            const response = await fetch(`../view/${view}`);
+            if (!response.ok) throw new Error("Erro ao carregar view");
+            const html = await response.text();
+            contentArea.innerHTML = html;
+        } catch (error) {
+            contentArea.innerHTML = `<div class="alert alert-danger">Erro ao carregar conteúdo da view</div>`;
+        }
+    }
+
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get("page");
+
+    // Usa operador ternário
+    page ? loadView(page) : contentArea.innerHTML = `<div class="text-center">Selecione uma view no menu</div>`;
+});
+    
